1a843da3591925dca3bde1f99f4d964e4f054f3e
Karim Guettech <karimguettech@gmail.com>
2023-03-15 00:02:17 +0100

    hf

---
+++ src/Auto/Automobile.java
@@ -0,0 +1,15 @@
+package Auto;
+
+public class Automobile {
+    int ruote;
+    int passeggeri;
+    public Automobile(int ruote, int passeggeri){
+        this.ruote=ruote;
+        this.passeggeri=passeggeri;
+    }
+    public int getRuote(){
+        return ruote;
+    }
+
+}
+
