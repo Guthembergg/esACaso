54581b22c852c11cd9e19c68e5b50134371c4d06
Karim Guettech <karimguettech@gmail.com>
2023-03-15 00:01:18 +0100

    hf

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import Auto.Automobile;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("!!!");
+        Automobile c3= new Automobile(5,3);
+        System.out.println(c3.getRuote());
     }
+
+
+
 }
